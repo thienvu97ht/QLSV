77b5dbbf9fb5f91203bf1fccc4f6556a3e577568
ThienVu97ht <thien.vu97ht123@gmail.com>
2021-04-04 19:01:26 +0700

    QLSV final

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 var sinhVienApi = '  http://localhost:3000/sinhVien';
+var overlayElement = document.querySelector('.overlay');
+var formElement = document.querySelector('.main-form');
+
+var createBtn = document.querySelector('#create');
+var btnUpdate = document.querySelector('#update');
+
+var nameInputElement = document.querySelector('#name');
+var emailInputElement = document.querySelector('#email');
+var phoneInputElement = document.querySelector('#phone');
+var namInputElement = document.querySelector('#male');
+var nuInputElement = document.querySelector('#female');
 
 function start() {
-    getSv(function(data) {
+    overlayElement.style.display = 'none';
+    formElement.style.display = 'none';
+    document.querySelector('#update').style.display = 'none';
+    getSv(function (data) {
         renderSinhVien(data)
     });
 }
@@ -10,7 +24,7 @@ start();
 
 // Functions
 
-// Lấy danh sách khóa học từ API
+// Lấy danh sách sinh viên từ API
 function getSv(callback) {
     fetch(sinhVienApi)
         .then(function (response) {
@@ -23,18 +37,17 @@ function getSv(callback) {
 function renderSinhVien(ListSinhVien) {
     var listSvBlock = document.querySelector('#list-sv');
     var htmls = ListSinhVien.map(function (sinhVien) {
-        console.log(sinhVien.id)
         return `
         <tr class="sv-item-${sinhVien.id}">
-            <td>${sinhVien.name}</td>
-            <td>${sinhVien.email}</td>
-            <td>${sinhVien.phone}</td>
-            <td>${sinhVien.gender}</td>
+            <td class="col1">${sinhVien.name}</td>
+            <td class="col2">${sinhVien.email}</td>
+            <td class="col3">${sinhVien.phone}</td>
+            <td class="col4">${sinhVien.gender}</td>
             <td>
-                <a class="edit" title="Edit" onclick="handleUpdateCourse(${sinhVien.id})">
+                <a class="edit" title="Edit" onclick="handleUpdateSv(${sinhVien.id})">
                     <i class="material-icons">&#xE254;</i>
                 </a>
-                <a class="delete" title="Delete" onclick="handleDeleteCourse(${sinhVien.id})">
+                <a class="delete" title="Delete" onclick="handleDeleteSv(${sinhVien.id})">
                     <i class="material-icons">&#xE872;</i>
                 </a>
             </td>
@@ -42,4 +55,170 @@ function renderSinhVien(ListSinhVien) {
         `
     });
     listSvBlock.innerHTML = htmls.join('');
-}
+};
+
+// Hàm tạo mới dữ liệu vào database
+function createSv(data, callback) {
+    var options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+    fetch(sinhVienApi, options)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(callback)
+};
+
+// Hàm sửa dữ liệu
+function updateSv(data, id) {
+    var options = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+    fetch(sinhVienApi + '/' + id, options)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function () {
+            getSv(renderSinhVien);
+        })
+};
+
+// Hàm xóa dữ liệu trong database
+function handleDeleteSv(id) {
+    var options = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    };
+    fetch(sinhVienApi + '/' + id, options)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function () {
+            var svItem = document.querySelector('.sv-item-' + id);
+            if (svItem) {
+                svItem.remove();
+            }
+        });
+};
+
+// Hàm xử lý lấy dữ liệu trong form
+function handleCreateForm(data) {
+    var name = nameInputElement.value;
+    var email = emailInputElement.value;
+    var phone = phoneInputElement.value;
+    var gender;
+
+    namInputElement.checked ? gender = 'Nam' : undefined;
+    nuInputElement.checked ? gender = 'Nữ' : undefined;
+
+    var data = {
+        name,
+        email,
+        phone,
+        gender
+    }
+
+    createSv(data, function () {
+        getSv(renderSinhVien);
+    });
+    document.querySelector('.main-form').style.display = 'none';
+    document.querySelector('.overlay').style.display = 'none';
+
+    nameInputElement.value = '';
+    emailInputElement.value = '';
+    phoneInputElement.value = '';
+    namInputElement.checked = false;
+    nuInputElement.checked = false;
+};
+
+// Hàm lấy value khóa học cũ
+function handleUpdateSv(id) {
+    showForm();
+    var itemSv = document.querySelector('.sv-item-' + id);
+
+    var nameSv = itemSv.querySelector('.col1');
+    var emailSv = itemSv.querySelector('.col2');
+    var phoneSv = itemSv.querySelector('.col3');
+    var genderSv = itemSv.querySelector('.col4');
+
+
+    nameInputElement.value = nameSv.innerHTML;
+    emailInputElement.value = emailSv.innerHTML;
+    phoneInputElement.value = phoneSv.innerHTML;
+    genderSv.innerHTML == 'Nam' ? namInputElement.checked = true : undefined;
+    genderSv.innerHTML == 'Nữ' ? nuInputElement.checked = true : undefined;
+
+    createBtn.style.display = 'none';
+    btnUpdate.style.display = 'block';
+
+    btnUpdate.onclick = function () {
+        var newName = document.querySelector('input[name="name"]');
+        var newEmail = document.querySelector('input[name="email"]');
+        var newPhone = document.querySelector('input[name="phone"]');
+        var newGender;
+
+        namInputElement.checked ? newGender = 'Nam' : undefined;
+        nuInputElement.checked ? newGender = 'Nữ' : undefined;
+
+        var formData = {
+            name: newName.value,
+            email: newEmail.value,
+            phone: newPhone.value,
+            gender: newGender
+        }
+
+        if (formData) {
+            updateSv(formData, id);
+        }
+
+
+        nameInputElement.value = '';
+        emailInputElement.value = '';
+        phoneInputElement.value = '';
+        namInputElement.checked = false;
+        nuInputElement.checked = false;
+
+        createBtn.style.display = 'block';
+        btnUpdate.style.display = 'none';
+        hideForm();
+    }
+
+
+
+
+};
+
+// Hàm nút bấm ẩn hiện form
+function showForm() {
+
+    if (overlayElement.style.display == 'none' || formElement.style.display == 'none') {
+        overlayElement.style.display = 'block';
+        formElement.style.display = 'block';
+    }
+};
+
+function hideForm() {
+    if (overlayElement.style.display == 'block' || formElement.style.display == 'block') {
+        overlayElement.style.display = 'none';
+        formElement.style.display = 'none';
+
+        nameInputElement.value = '';
+        emailInputElement.value = '';
+        phoneInputElement.value = '';
+        namInputElement.checked = false;
+        nuInputElement.checked = false;
+
+        createBtn.style.display = 'block';
+        btnUpdate.style.display = 'none';
+    }
+};
